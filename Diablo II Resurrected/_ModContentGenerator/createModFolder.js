e071eb61eabc4e8039837df77d2d85b5a8670c3b
Kikiwako <vingi20@hotmail.com>
2026-01-09 22:05:41 -0500

    changed mod folder name

--- Diablo II Resurrected/_ModContentGenerator/createModFolder.js
+++ Diablo II Resurrected/_ModContentGenerator/createModFolder.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const generatedfolder = __dirname + "/generatedModFolder";
+const generatedfolder = __dirname + "/mod";
 
 const MAIN = async () => {
     const folderList = getFolderListOfParentDirectory();
